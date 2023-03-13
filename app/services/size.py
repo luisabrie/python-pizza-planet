ee2355b120d484fa0060cfb71bbf14dd53ed7626
luisabrie <luis.abrie@gmail.com>
2023-03-13 17:07:00 -0500

    fix: PIZ-1 added get pizza sizes endpoint

--- app/services/size.py
+++ app/services/size.py
@@ -5,6 +5,19 @@ from ..controllers import SizeController
 
 size = Blueprint('size', __name__)
 
+@size.route('/', methods=GET)
+def get_sizes():
+    sizes, error = SizeController.get_all()
+    response = sizes if not error else {'error': error}
+    status_code = 200 if sizes else 404 if not error else 400
+    return jsonify(response), status_code
+
+@size.route('/id/<_id>', methods=GET)
+def get_size_by_id(_id: int):
+    size, error = SizeController.get_by_id(_id)
+    response = size if not error else {'error': error}
+    status_code = 200 if size else 404 if not error else 400
+    return jsonify(response), status_code
 
 @size.route('/', methods=POST)
 def create_size():
@@ -12,7 +25,6 @@ def create_size():
     response = size if not error else {'error': error}
     status_code = 200 if not error else 400
     return jsonify(response), status_code
-
 
 @size.route('/', methods=PUT)
 def update_size():
@@ -22,9 +34,3 @@ def update_size():
     return jsonify(response), status_code
 
 
-@size.route('/id/<_id>', methods=GET)
-def get_size_by_id(_id: int):
-    size, error = SizeController.get_by_id(_id)
-    response = size if not error else {'error': error}
-    status_code = 200 if size else 404 if not error else 400
-    return jsonify(response), status_code
