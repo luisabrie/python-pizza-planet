c09d1c86eb5a0be2b63d03f2d379afa59a510723
luisabrie <luis.abrie@gmail.com>
2023-03-14 16:01:57 -0500

    feat: PIZ-2 add beverages functionalities

--- app/repositories/managers.py
+++ app/repositories/managers.py
@@ -2,8 +2,8 @@ from typing import Any, List, Optional, Sequence
 
 from sqlalchemy.sql import text, column
 
-from .models import Ingredient, Order, IngredientDetail, Size, db
-from .serializers import (IngredientSerializer, OrderSerializer,
+from .models import Ingredient, Beverage, Order, IngredientDetail, BeverageDetail, Size, db
+from .serializers import (IngredientSerializer, OrderSerializer, BeverageSerializer,
                           SizeSerializer, ma)
 
 
@@ -43,6 +43,13 @@ class SizeManager(BaseManager):
     model = Size
     serializer = SizeSerializer
 
+class BeverageManager(BaseManager):
+    model = Beverage
+    serializer = BeverageSerializer
+
+    @classmethod
+    def get_by_id_list(cls, ids: Sequence):
+        return cls.session.query(cls.model).filter(cls.model._id.in_(set(ids))).all() or []
 
 class IngredientManager(BaseManager):
     model = Ingredient
@@ -58,13 +65,15 @@ class OrderManager(BaseManager):
     serializer = OrderSerializer
 
     @classmethod
-    def create(cls, order_data: dict, ingredients: List[Ingredient]):
+    def create(cls, order_data: dict, ingredients: List[Ingredient], beverages: List[Beverage]):
         new_order = cls.model(**order_data)
         cls.session.add(new_order)
         cls.session.flush()
         cls.session.refresh(new_order)
         cls.session.add_all((IngredientDetail(order_id=new_order._id, ingredient_id=ingredient._id, ingredient_price=ingredient.price)
                              for ingredient in ingredients))
+        cls.session.add_all((BeverageDetail(order_id=new_order._id, beverage_id=beverage._id, beverage_price=beverage.price) 
+                             for beverage in beverages))
         cls.session.commit()
         return cls.serializer().dump(new_order)
 
